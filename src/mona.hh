6764d2cd20c917ff1d688bfdef5898db4ebf8eba
Yong Li <duckly_lee@163.com>
2020-08-15 21:01:58 +0800

    add test for reverse and project functions

--- src/mona.hh
+++ src/mona.hh
@@ -32,3 +32,6 @@ str_split(string str, vector<string>& result, char delim = ' ');
 // translate ltlf formula to DFA via mona
 twa_graph_ptr
 translate_ltlf_mona(formula ltl, bdd_dict_ptr dict);
+
+twa_graph_ptr
+read_from_mona_file(const char * file_name, bdd_dict_ptr dict);
